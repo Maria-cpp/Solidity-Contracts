8d1f25e4ad18720be73e2d9cbf60d0636b5c4d52
Maria-cpp <vipisbd@gmail.com>
2021-11-08 15:08:30 +0500

    initial commit

---
+++ SimpleAuction.sol
@@ -0,0 +1,70 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract SimpleAuction{
+
+    address payable public beneficiary;
+
+    uint public auctionEndTime;
+
+    address public highestBidder;
+
+    uint public highestBid;
+
+    mapping(address => uint) pendingReturns;
+
+    bool ended;
+
+    event HighestBidIncreased(address bidder, uint amount);
+
+    event auctionEnded(address winner, uint amount);
+
+    constructor(
+
+        uint _biddingTime,
+
+        address payable _beneficiary
+
+    ) public{
+
+        beneficiary = _beneficiary;
+
+        auctionEndTime = now+ _biddingTime;
+
+    }
+
+    function bid() public payable{
+
+        require(
+            now <= auctionEndTime, "The auction has ended!");
+
+        require(
+            msg.value > highestBid, "There is a higher bid!");
+
+        if(highestBid !=0) {
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBidder = msg.sender;
+
+        highestBid = msg.value;
+
+        emit HighestBidIncreased(msg.sender, msg.value);
+    }
+
+
+    function withdraw() public returns (bool){
+
+        uint amount = pendingReturns[msg.sender];
+        if(amount > 0){
+            pendingReturns[msg.sender] = 0;
+
+            if (!msg.sender.send(amount)) {
+
+                    pendingReturns[msg.sender] = amount;
+                    return false;
+                }
+            }
+        return true;
+    }
+
+}
